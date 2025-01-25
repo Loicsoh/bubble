e31fc127c90c4112b8d8c15424ffa6a4cd4feb5f
Loicsoh <www.sohloic132@gmail.com>
2025-01-25 12:17:18 +0100

    creation du bubble

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const counterDisplay = document.querySelector('h3');
 let counter = 0;
-let interval = setInterval(() => {
-    counterDisplay.textContent = counter;
-});
+const bubble = document.createElement('span');
+bubble.classList.add('bubble');
+document.body.appendChild(bubble);
+// console.log(Math.random()*12 + 100 + 'px');
+
+const size =`${Math.random()*200 + 100}px`;
+
+bubble.style.width = size;
+bubble.style.height = size;
+
+console.log(size);
